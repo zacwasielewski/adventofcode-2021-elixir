b3d29d08ee8e8a09232e9d73ec42b32346c20554
Zac Wasielewski <zac@wasielewski.org>
2021-12-01 10:56:20 -0500

    Refactor Day 1 with more pipelines to eliminate unnecessary named variables

--- day-01/solve.exs
+++ day-01/solve.exs
@@ -8,36 +8,40 @@ defmodule Day1 do
 		|> String.split("\n", trim: true)
 		|> Enum.map(&String.to_integer/1)
   end
-
+	
 	@doc """
 		Given an array of integer depth values, count the number of times a depth measurement increases from the previous measurement.
 	"""
 	def count_depth_increases(input) do
-		pairs = Enum.chunk_every(input, 2, 1, :discard)
-		Enum.count(pairs, fn [a, b] -> b > a end)
+		input
+		|> Enum.chunk_every(2, 1, :discard)
+		|> Enum.count(fn [a, b] -> b > a end)
 	end
 
 	@doc """
 		Group an array of integers into a sliding "window", and sum the total of each group.
 	"""		
-	def sum_sliding_window(input, window) do
-		window = Enum.chunk_every(input, window, 1, :discard)
-		Enum.map(window, fn x -> Enum.sum(x) end)
+	def sum_sliding_window(input, window_length) do
+		input
+		|> Enum.chunk_every(window_length, 1, :discard)
+		|> Enum.map(fn x -> Enum.sum(x) end)
 	end
 		
 	@doc """
 		How many depth measurements are larger than the previous measurement?
 	"""
 	def part1 do
-		count_depth_increases get_input()
+		get_input()
+		|> count_depth_increases()
 	end
 			
 	@doc """
 		Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
 	"""
 	def part2 do
-		windowed_input = sum_sliding_window(get_input(), 3)
-		count_depth_increases(windowed_input)
+		get_input()
+		|> sum_sliding_window(3)
+		|> count_depth_increases()
 	end
 end
 
