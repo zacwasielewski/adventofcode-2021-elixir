1a3038ed3df528197b269a5f35186e74e2bf0f6c
Zac Wasielewski <zac@wasielewski.org>
2021-12-01 08:15:21 -0500

    Day 1, Part 2 solution

--- day-01/solve.exs
+++ day-01/solve.exs
@@ -9,18 +9,31 @@ defmodule Day1 do
 		|> Enum.map(&String.to_integer/1)
   end
 
-	
 	def count_depth_increases(input) do
 		pairs = Enum.chunk_every(input, 2, 1, :discard)
 		Enum.count(pairs, fn [a, b] -> b > a end)
 	end
 	
+	def sum_sliding_window(input) do
+		window = Enum.chunk_every(input, 3, 1, :discard)
+		Enum.map(window, fn x -> Enum.sum(x) end)
+	end
+		
 	@doc """
 		How many measurements are larger than the previous measurement?
 	"""
 	def part1 do
 		count_depth_increases get_input()
 	end
+			
+	@doc """
+		How many measurements are larger than the previous measurement?
+	"""
+	def part2 do
+		windowed_input = Day1.sum_sliding_window get_input()
+		count_depth_increases(windowed_input)
+	end
 end
 
 IO.puts "Part 1: #{Day1.part1}"
+IO.puts "Part 2: #{Day1.part2}"
--- day-01/test.exs
+++ day-01/test.exs
@@ -2,20 +2,30 @@ ExUnit.start
 
 defmodule Day1Test do
   use ExUnit.Case
-
-  test "part_1_test_1" do
-  	input = [
-      199,
-      200,
-      208,
-      210,
-      200,
-      207,
-      240,
-      269,
-      260,
-      263,
-    ]
-  	assert Day1.count_depth_increases(input) === 7
+  
+  @example_input [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263,
+  ]
+  
+  test "count_depth_increases_example" do
+    assert Day1.count_depth_increases(@example_input) === 7
+  end
+    
+  test "sum_sliding_window_example" do
+    assert Day1.sum_sliding_window(@example_input) === [ 607, 618, 618, 617, 647, 716, 769, 792 ]
+  end
+    
+  test "part_2_example" do
+    windowed_input = Day1.sum_sliding_window(@example_input)
+    assert Day1.count_depth_increases(windowed_input) === 5
   end
 end
