0f631b187699fec518904ae92c652d50c557fe4c
Zac Wasielewski <zac@wasielewski.org>
2021-12-02 08:41:30 -0500

    Gross solution to Day 2, Part 2

--- day-02/solution.exs
+++ day-02/solution.exs
@@ -19,14 +19,35 @@ defmodule Day2 do
       "up" -> [ horizontal, depth - amount]
     end
   end
-  
+
+  def next_position_aimed(position, move) do
+    %{ aim: aim, depth: depth, horizontal: horizontal } = position
+    [ direction, amount_ ] = String.split(move, " ")
+    amount = String.to_integer(amount_)
+    
+    case direction do
+      "down" -> %{position | aim: aim + amount}
+      "up" -> %{position | aim: aim - amount}
+      "forward" -> %{position | horizontal: horizontal + amount, depth: depth + aim * amount}
+    end
+  end
+    
   def calculate_position(input) do
     initial = [0, 0]
     Enum.reduce(input, initial, fn move, acc -> next_position(acc, move) end)
   end
-  
+      
+  def calculate_position_aimed(input) do
+    initial = %{ horizontal: 0, depth: 0, aim: 0 }
+    Enum.reduce(input, initial, fn move, acc -> next_position_aimed(acc, move) end)
+  end
+    
   def calculate_position_product(position) do
     Enum.at(position, 0) * Enum.at(position, 1)
+  end
+    
+  def calculate_position_product_aimed(position) do    
+    position[:horizontal] * position[:depth]
   end
     
   def part1 do
@@ -34,7 +55,14 @@ defmodule Day2 do
     |> calculate_position
     |> calculate_position_product
   end
+
+  def part2 do
+    get_input()
+    |> calculate_position_aimed
+    |> calculate_position_product_aimed
+  end
+  
 end
 
 IO.puts "Part 1: #{Day2.part1}"
-#IO.puts "Part 2: #{Day2.part2}"
+IO.puts "Part 2: #{Day2.part2}"
--- day-02/tests.exs
+++ day-02/tests.exs
@@ -3,28 +3,17 @@ ExUnit.start
 defmodule Day2Test do
   use ExUnit.Case
   
-  #@example_input [
-  #  199,
-  #  200,
-  #  208,
-  #  210,
-  #  200,
-  #  207,
-  #  240,
-  #  269,
-  #  260,
-  #  263,
-  #]
+  @example_input [
+    "forward 5",
+    "down 5",
+    "forward 8",
+    "up 3",
+    "down 8",
+    "forward 2"
+  ]
   
   test "part_1_example" do
-    input = [
-      "forward 5",
-      "down 5",
-      "forward 8",
-      "up 3",
-      "down 8",
-      "forward 2"
-    ]
+    input = @example_input
     
     assert [ 15, 10 ] ==
       input
@@ -34,5 +23,18 @@ defmodule Day2Test do
       input
       |> Day2.calculate_position
       |> Day2.calculate_position_product
+  end
+  
+  test "part_2_example" do
+    input = @example_input
+    
+    assert %{aim: 10, depth: 60, horizontal: 15} ==
+      input
+      |> Day2.calculate_position_aimed
+  
+    assert 900 ==
+      input
+      |> Day2.calculate_position_aimed
+      |> Day2.calculate_position_product_aimed
   end
 end
