64e07cc87f53de8f51d70febd950558b656980d4
Zac Wasielewski <zac@wasielewski.org>
2021-12-02 13:11:23 -0500

    Refactor Day 2 to use modules and avoid some code repetition

--- day-02/solution.exs
+++ day-02/solution.exs
@@ -1,17 +1,25 @@
 defmodule Day2 do
   @input_file "day-02/input.txt"
   
-  defp get_input do
-    {:ok, input} = File.read(@input_file)
-    
+  def get_input do
+    {:ok, input} = File.read(@input_file)    
+    parse_input(input)
+  end
+  
+  def parse_input(input) do
     input
     |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> String.split(line, " ") end)
+    |> Enum.map(fn [direction, amount] -> [direction, String.to_integer(amount)] end)
   end
+end
+
+defmodule Day2.Part1 do
+  import Day2
   
   def next_position(current, move) do
     [ horizontal, depth ] = current
-    [ direction, amount_string ] = String.split(move, " ")
-    amount = String.to_integer(amount_string)
+    [ direction, amount ] = move
     
     case direction do
       "forward" -> [ horizontal + amount, depth ]
@@ -19,11 +27,29 @@ defmodule Day2 do
       "up" -> [ horizontal, depth - amount]
     end
   end
+    
+  def calculate_position(input) do
+    initial = [0, 0]
+    Enum.reduce(input, initial, fn move, acc -> next_position(acc, move) end)
+  end
+    
+  def calculate_solution(position) do
+    Enum.at(position, 0) * Enum.at(position, 1)
+  end
 
-  def next_position_aimed(position, move) do
+  def solve do
+    get_input()
+    |> calculate_position
+    |> calculate_solution
+  end  
+end
+
+defmodule Day2.Part2 do
+  import Day2
+
+  def next_position(position, move) do
     %{ aim: aim, depth: depth, horizontal: horizontal } = position
-    [ direction, amount_ ] = String.split(move, " ")
-    amount = String.to_integer(amount_)
+    [ direction, amount ] = move
     
     case direction do
       "down" -> %{position | aim: aim + amount}
@@ -31,38 +57,22 @@ defmodule Day2 do
       "forward" -> %{position | horizontal: horizontal + amount, depth: depth + aim * amount}
     end
   end
-    
+
   def calculate_position(input) do
-    initial = [0, 0]
+    initial = %{ horizontal: 0, depth: 0, aim: 0 }
     Enum.reduce(input, initial, fn move, acc -> next_position(acc, move) end)
   end
-      
-  def calculate_position_aimed(input) do
-    initial = %{ horizontal: 0, depth: 0, aim: 0 }
-    Enum.reduce(input, initial, fn move, acc -> next_position_aimed(acc, move) end)
-  end
-    
-  def calculate_position_product(position) do
-    Enum.at(position, 0) * Enum.at(position, 1)
-  end
-    
-  def calculate_position_product_aimed(position) do    
+
+  def calculate_solution(position) do    
     position[:horizontal] * position[:depth]
   end
-    
-  def part1 do
+  
+  def solve do
     get_input()
     |> calculate_position
-    |> calculate_position_product
+    |> calculate_solution
   end
-
-  def part2 do
-    get_input()
-    |> calculate_position_aimed
-    |> calculate_position_product_aimed
-  end
-  
 end
 
-IO.puts "Part 1: #{Day2.part1}"
-IO.puts "Part 2: #{Day2.part2}"
+IO.puts "Part 1: #{Day2.Part1.solve}"
+IO.puts "Part 2: #{Day2.Part2.solve}"
--- day-02/tests.exs
+++ day-02/tests.exs
@@ -3,38 +3,38 @@ ExUnit.start
 defmodule Day2Test do
   use ExUnit.Case
   
-  @example_input [
-    "forward 5",
-    "down 5",
-    "forward 8",
-    "up 3",
-    "down 8",
-    "forward 2"
-  ]
+  @example_input """
+    forward 5
+    down 5
+    forward 8
+    up 3
+    down 8
+    forward 2
+    """
   
   test "part_1_example" do
-    input = @example_input
-    
+    input = Day2.parse_input(@example_input)    
+
     assert [ 15, 10 ] ==
       input
-      |> Day2.calculate_position
+      |> Day2.Part1.calculate_position
 
     assert 150 ==
       input
-      |> Day2.calculate_position
-      |> Day2.calculate_position_product
+      |> Day2.Part1.calculate_position
+      |> Day2.Part1.calculate_solution
   end
   
   test "part_2_example" do
-    input = @example_input
+    input = Day2.parse_input(@example_input)
     
     assert %{aim: 10, depth: 60, horizontal: 15} ==
       input
-      |> Day2.calculate_position_aimed
+      |> Day2.Part2.calculate_position
   
     assert 900 ==
       input
-      |> Day2.calculate_position_aimed
-      |> Day2.calculate_position_product_aimed
+      |> Day2.Part2.calculate_position
+      |> Day2.Part2.calculate_solution
   end
 end
