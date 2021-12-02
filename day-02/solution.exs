055a74b1ce83b793a924d9d1a6e11fb727c44783
Zac Wasielewski <zac@wasielewski.org>
2021-12-02 07:02:53 -0500

    Scaffold Day 2 files

---
+++ day-02/solution.exs
@@ -0,0 +1,45 @@
+defmodule Day2 do
+  @input_file "day-02/input.txt"
+  
+  defp get_input do
+    {:ok, input} = File.read(@input_file)
+    
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+  
+  #@doc """
+  #  Given an array of integer depth values, count the number of times a depth measurement increases from the previous measurement.
+  #"""
+  #def count_depth_increases(input) do
+  #  input
+  #  |> Enum.chunk_every(2, 1, :discard)
+  #  |> Enum.count(fn [a, b] -> b > a end)
+  #end
+
+  #@doc """
+  #  Group an array of integers into a sliding "window", and sum the total of each group.
+  #"""    
+  #def sum_sliding_window(input, window_length) do
+  #  input
+  #  |> Enum.chunk_every(window_length, 1, :discard)
+  #  |> Enum.map(fn x -> Enum.sum(x) end)
+  #end
+    
+  # How many depth measurements are larger than the previous measurement?
+  def part1 do
+    #get_input()
+    #|> count_depth_increases()
+  end
+      
+  # Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+  def part2 do
+    #get_input()
+    #|> sum_sliding_window(3)
+    #|> count_depth_increases()
+  end
+end
+
+IO.puts "Part 1: #{Day1.part1}"
+IO.puts "Part 2: #{Day1.part2}"
---
+++ day-02/tests.exs
@@ -0,0 +1,31 @@
+ExUnit.start
+
+defmodule Day2Test do
+  use ExUnit.Case
+  
+  #@example_input [
+  #  199,
+  #  200,
+  #  208,
+  #  210,
+  #  200,
+  #  207,
+  #  240,
+  #  269,
+  #  260,
+  #  263,
+  #]
+  
+  test "count_depth_increases_example" do
+    #assert Day1.count_depth_increases(@example_input) === 7
+  end
+    
+  test "sum_sliding_window_example" do
+    #assert Day1.sum_sliding_window(@example_input, 3) === [ 607, 618, 618, 617, 647, 716, 769, 792 ]
+  end
+    
+  test "part_2_example" do
+    #windowed_input = Day1.sum_sliding_window(@example_input, 3)
+    #assert Day1.count_depth_increases(windowed_input) === 5
+  end
+end
