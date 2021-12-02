f962b35ea620eb310acce1ac1910972fa201d881
Zac Wasielewski <zac@wasielewski.org>
2021-12-02 15:57:01 -0500

    Alternate solution to part 1 without reduce()

--- day-02/solution.exs
+++ day-02/solution.exs
@@ -2,50 +2,49 @@ defmodule Day2 do
   @input_file "day-02/input.txt"
   
   def get_input do
-    {:ok, input} = File.read(@input_file)    
-    parse_input(input)
-  end
-  
-  def parse_input(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn line -> String.split(line, " ") end)
-    |> Enum.map(fn [direction, amount] -> [direction, String.to_integer(amount)] end)
+    {:ok, input} = File.read(@input_file)
+    input |> String.split("\n", trim: true)
   end
 end
 
 defmodule Day2.Part1 do
   import Day2
   
-  def next_position(current, move) do
-    [ horizontal, depth ] = current
-    [ direction, amount ] = move
-    
-    case direction do
-      "forward" -> [ horizontal + amount, depth ]
-      "down" -> [ horizontal, depth + amount]
-      "up" -> [ horizontal, depth - amount]
-    end
+  def calculate_depth(input) do
+    input
+    |> Enum.filter(fn x -> String.starts_with?(x, ["up", "down"]) end)
+    |> Enum.map(fn x -> String.replace_prefix(x, "up ", "-") end)
+    |> Enum.map(fn x -> String.replace_prefix(x, "down ", "") end)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
+  
+  def calculate_horizontal(input) do
+    input
+    |> Enum.filter(fn x -> String.starts_with?(x, "forward") end)
+    |> Enum.map(fn x -> String.replace_prefix(x, "forward ", "") end)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
     
-  def calculate_position(input) do
-    initial = [0, 0]
-    Enum.reduce(input, initial, fn move, acc -> next_position(acc, move) end)
-  end
-    
-  def calculate_solution(position) do
-    Enum.at(position, 0) * Enum.at(position, 1)
+  def calculate_solution(input) do
+    calculate_depth(input) * calculate_horizontal(input)
   end
 
   def solve do
     get_input()
-    |> calculate_position
     |> calculate_solution
   end  
 end
 
 defmodule Day2.Part2 do
   import Day2
+    
+  def parse_input(input) do
+    input
+    |> Enum.map(fn line -> String.split(line, " ") end)
+    |> Enum.map(fn [direction, amount] -> [direction, String.to_integer(amount)] end)
+  end
 
   def next_position(position, move) do
     %{ aim: aim, depth: depth, horizontal: horizontal } = position
@@ -69,6 +68,7 @@ defmodule Day2.Part2 do
   
   def solve do
     get_input()
+    |> parse_input
     |> calculate_position
     |> calculate_solution
   end
--- day-02/tests.exs
+++ day-02/tests.exs
@@ -10,23 +10,18 @@ defmodule Day2Test do
     up 3
     down 8
     forward 2
-    """
+    """ |> String.split("\n", trim: true)
   
   test "part_1_example" do
-    input = Day2.parse_input(@example_input)    
+    input = @example_input
 
-    assert [ 15, 10 ] ==
-      input
-      |> Day2.Part1.calculate_position
-
-    assert 150 ==
-      input
-      |> Day2.Part1.calculate_position
-      |> Day2.Part1.calculate_solution
+    assert 15  == input |> Day2.Part1.calculate_horizontal
+    assert 10  == input |> Day2.Part1.calculate_depth
+    assert 150 == input |> Day2.Part1.calculate_solution
   end
   
   test "part_2_example" do
-    input = Day2.parse_input(@example_input)
+    input = Day2.Part2.parse_input(@example_input)
     
     assert %{aim: 10, depth: 60, horizontal: 15} ==
       input
