13d9c024c7fe8a2e738127c148e64701aa1de9c5
Zac Wasielewski <zac@wasielewski.org>
2021-12-03 13:40:38 -0500

    Day 3 Part 1 solution

--- day-03/solution.exs
+++ day-03/solution.exs
@@ -8,19 +8,54 @@ defmodule Day3 do
 end
 
 defmodule Day3.Part1 do
-  def parse_input(input) do
+  def transpose(rows) do
+    rows
+    |> List.zip
+    |> Enum.map(&Tuple.to_list/1)
+  end
+  
+  def flip_bit(bit) do
+    case bit do
+      0 -> 1
+      1 -> 0
+    end
+  end
+  
+  def binary_string_to_decimal(s) do
+    String.to_integer(s, 2)
+  end
+
+  def gamma(input) do    
     input
-    |> Enum.map(fn line -> String.split(line, " ") end)
-    |> Enum.map(fn [direction, amount] -> [direction, String.to_integer(amount)] end)
+    # Convert the input to a (pseudo) matrix of integers
+    |> Enum.map(fn x -> String.graphemes(x) |> Enum.map(&String.to_integer/1) end)
+    # Swap the rows and columns
+    |> transpose
+    # Get the most frequent bit in each "column"
+    |> Enum.map(&Enum.frequencies/1)
+    |> Enum.map(fn x -> Enum.max_by(x, fn {_k, v} -> v end) |> elem(0) end)
+    # Convert the result back to a string
+    |> Enum.join
+  end
+  
+  def epsilon(input) do
+    gamma(input)
+    |> String.graphemes
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&flip_bit/1)
+    |> Enum.join
+  end
+  
+  def power_consumption(gamma, epsilon) do
+    gamma_decimal = binary_string_to_decimal(gamma)
+    epsilon_decimal = binary_string_to_decimal(epsilon)
+    gamma_decimal * epsilon_decimal
   end
   
   def solve(input) do
-    forward = sum_moves_by_direction(input, "forward")
-    up = sum_moves_by_direction(input, "up")
-    down = sum_moves_by_direction(input, "down")
-    depth = down - up
-    
-    forward * depth
+    gamma = gamma(input)
+    epsilon = epsilon(input)
+    power_consumption(gamma, epsilon)
   end
 end
 
--- day-03/tests.exs
+++ day-03/tests.exs
@@ -1,22 +1,43 @@
 ExUnit.start
 
 defmodule Day3Test do
-  use ExUnit.Case
-  
-  @example_input """
-    forward 5
-    down 5
-    forward 8
-    up 3
-    down 8
-    forward 2
-    """ |> String.split("\n", trim: true)
-  
-  test "part_1_example" do
-    input = @example_input
-
+  defmodule Part1 do
+    use ExUnit.Case
+    import Day3.Part1
     
+    @example_input """
+      00100
+      11110
+      10110
+      10111
+      10101
+      01111
+      00111
+      11100
+      10000
+      11001
+      00010
+      01010
+      """ |> String.split("\n", trim: true)
+    
+    test "part_1_example" do
+      input = @example_input
+  
+      gamma   = gamma(input)
+      epsilon = epsilon(input)
+      
+      assert gamma == "10110"
+      assert epsilon == "01001"
+      
+      gamma_decimal   = binary_string_to_decimal(gamma)
+      epsilon_decimal = binary_string_to_decimal(epsilon)
 
-    #assert 150 == input |> Day2.Part1.solve
+      assert gamma_decimal == 22
+      assert epsilon_decimal == 9
+
+      assert power_consumption(gamma, epsilon) === 198
+      
+      assert 198 == input |> Day3.Part1.solve
+    end
   end
 end
