53c626c419fe7fd957a742d5c592480802f945bc
Zac Wasielewski <zac@wasielewski.org>
2021-12-03 22:07:16 -0500

    Day 3 Part 2 solution

--- day-03/solution.exs
+++ day-03/solution.exs
@@ -59,5 +59,107 @@ defmodule Day3.Part1 do
   end
 end
 
+defmodule Day3.Part2 do
+  def to_matrix(input) do
+    input
+    |> Enum.map(fn x -> String.graphemes(x) |> Enum.map(&String.to_integer/1) end)    
+  end
+    
+  def transpose(rows) do
+    rows
+    |> List.zip
+    |> Enum.map(&Tuple.to_list/1)
+  end
+  
+  def most_frequent(list, tiebreaker) do
+    f = Enum.frequencies(list)
+    cond do
+      f[0] >  f[1] -> 0
+      f[0] <  f[1] -> 1
+      f[0] == f[1] -> tiebreaker
+    end
+  end
+  
+  def least_frequent(list, tiebreaker) do
+    f = Enum.frequencies(list)
+    cond do
+      f[0] >  f[1] -> 1
+      f[0] <  f[1] -> 0
+      f[0] == f[1] -> tiebreaker
+    end
+  end
+
+  def binary_string_to_decimal(s) do
+    String.to_integer(s, 2)
+  end
+  
+  def filter_by_bit(input, position, bit) do
+    Enum.filter(input, fn x -> String.at(x, position) == bit end)
+  end
+
+  def gamma(input) do    
+    input
+    |> to_matrix
+    |> transpose
+    |> Enum.map(fn x -> most_frequent(x, 1) end)
+    |> Enum.join
+  end
+  
+  def epsilon(input) do
+    input
+    |> to_matrix
+    |> transpose
+    |> Enum.map(fn x -> least_frequent(x, 0) end)
+    |> Enum.join
+  end
+  
+  def oxygen_generator_rating(input) do
+    gamma(input)
+    |> String.graphemes
+    |> Enum.with_index
+    |> Enum.reduce_while(input, fn ({_, index}, acc) ->
+      gamma = gamma(acc)
+      bit = String.at(gamma, index)
+      filtered = filter_by_bit(acc, index, bit)
+      
+      if Enum.count(filtered) == 1 do
+        {:halt, Enum.at(filtered, 0)}
+      else
+        {:cont, filtered}
+      end
+    end)
+  end
+
+  def co2_scrubber_rating(input) do
+    epsilon(input)
+    |> String.graphemes
+    |> Enum.with_index
+    |> Enum.reduce_while(input, fn ({_, index}, acc) ->
+      epsilon = epsilon(acc)
+      bit = String.at(epsilon, index)
+      filtered = filter_by_bit(acc, index, bit)
+      
+      if Enum.count(filtered) == 1 do
+        {:halt, Enum.at(filtered, 0)}
+      else
+        {:cont, filtered}
+      end
+    end)
+  end
+
+  def life_support_rating(o2_rating, co2_rating) do
+    o2_decimal = binary_string_to_decimal(o2_rating)
+    co2_decimal = binary_string_to_decimal(co2_rating)
+    o2_decimal * co2_decimal    
+  end
+  
+  def solve(input) do
+    o2_rating = oxygen_generator_rating(input)
+    co2_rating = co2_scrubber_rating(input)
+    life_support_rating(o2_rating, co2_rating)    
+  end
+end
+
 input = Day3.get_input()
 IO.puts "Part 1: #{Day3.Part1.solve(input)}"
+IO.puts "Part 2: #{Day3.Part2.solve(input)}"
--- day-03/tests.exs
+++ day-03/tests.exs
@@ -24,20 +24,54 @@ defmodule Day3Test do
       input = @example_input
   
       gamma   = gamma(input)
-      epsilon = epsilon(input)
-      
-      assert gamma == "10110"
-      assert epsilon == "01001"
-      
+      epsilon = epsilon(input)      
       gamma_decimal   = binary_string_to_decimal(gamma)
       epsilon_decimal = binary_string_to_decimal(epsilon)
 
+      assert gamma == "10110"
+      assert epsilon == "01001"
       assert gamma_decimal == 22
       assert epsilon_decimal == 9
-
+      
       assert power_consumption(gamma, epsilon) === 198
       
       assert 198 == input |> Day3.Part1.solve
+    end
+  end
+
+  defmodule Part2 do
+    use ExUnit.Case
+    import Day3.Part2
+    
+    @example_input """
+      00100
+      11110
+      10110
+      10111
+      10101
+      01111
+      00111
+      11100
+      10000
+      11001
+      00010
+      01010
+      """ |> String.split("\n", trim: true)
+    
+    test "part_2_example" do
+      input = @example_input
+
+      o2_rating  = oxygen_generator_rating(input)
+      co2_rating = co2_scrubber_rating(input)
+      o2_rating_decimal = binary_string_to_decimal(o2_rating)
+      co2_rating_decimal = binary_string_to_decimal(co2_rating)
+            
+      assert o2_rating === "10111"
+      assert co2_rating === "01010"
+      assert o2_rating_decimal == 23
+      assert co2_rating_decimal == 10
+      
+      assert life_support_rating(o2_rating, co2_rating) === 230
     end
   end
 end
